b61aa634b38ad0f6e45e3f5057f8d08f0f928940
AndresMoreno <jamoreno33@misena.edu.co>
2021-10-01 19:43:24 -0500

    1ventas

---
+++ src/components/ventas/components/administrador_Venta/Aventas.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react"
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container'; 
+import TablaVentas from './TablaVentas';
+//import Tabla from './components/ventas/components/administrador_Venta/Tabla';
+//import Barra from './components/ventas/components/administrador_Venta/Barra';
+class ListaVentas extends Component {
+    render () {
+        return (
+            <Container>
+                <Row  className="d-flex justify-content-center align-items-center mt-5"> 
+                    <TablaVentas/> 
+                   
+                </Row> 
+            </Container>
+            
+        );
+    }
+}
+export default ListaVentas;
---
+++ src/components/ventas/components/administrador_Venta/TablaVentas.js
@@ -0,0 +1,41 @@
+import Table from 'react-bootstrap/Table'
+//import productos from '../../producto/ListaProductos.json';
+
+import misventas from '../misventas/ListaVentas.json';
+
+const TablaVentas = ()=>{
+    return(
+        <Table striped bordered hover variant="secondary">
+            <thead >
+            <tr>
+                <th scope="col">N.Venta</th>
+                <th scope="col">ID</th>
+                <th scope="col">Cantidad</th>
+                <th scope="col">Precio</th>
+                {/* <th scope="col">Imagen</th> */}
+                <th scope="col">Edita</th>
+                <th scope="col">Eliminar</th>
+            </tr>
+            </thead> 
+            <tbody>
+            
+            {misventas.map(item => (
+                
+            <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.nombre}</td>
+                <td>{item.stock}</td>
+                <td>${item.precio}</td>
+                <td><a className= "link"href="/Nproducto">Editar</a></td>
+                <td><a className= "link"href="/Home">Eliminar</a></td> 
+            </tr>
+
+            ))}
+
+            </tbody>
+
+    </Table>
+    )
+};
+
+export default  TablaVentas;
