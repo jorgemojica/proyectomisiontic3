b81eac6a7b602abe7d8488f2a2611376353317b5
Duvan Caicedo <duvamendi2@hotmail.com>
2021-10-07 20:39:44 -0500

    parte de la bases de datos

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,20 @@
-var express = require('express')
-var app = express()
+var express = require("express");
+var mongoose = require("mongoose");
+var app = express();
 
-const productsRouts = require("./routes/products");
+const productsRoutes = require("./routes/products");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/products", productsRouts);
+mongoose
+  .connect(
+    "mongodb+srv://duvan:735843.@cluster0.kench.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Estamos conectados");
+  });
+
+app.use("/api/products", productsRoutes);
 
 module.exports = app;
